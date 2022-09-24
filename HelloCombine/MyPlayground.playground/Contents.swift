b6081d3162b1af4bb7001ca91547370d07de3110
applebuddy <eagermin92@gmail.com>
2022-09-25 01:18:31 +0900

    Init, Study lecture 8. Hello Publishers and Subscribers
    could know sending notification using imparative programming examples

--- HelloCombine/MyPlayground.playground/Contents.swift
+++ HelloCombine/MyPlayground.playground/Contents.swift
@@ -2,6 +2,16 @@ import UIKit
 
 var greeting = "Hello, playground"
 print(greeting)
+
+// MARK: - 8. Hello Publishers and Subscribers
+// - Subscriber가 Publisher를 구독하면, Publisher는 데이터 이벤트를 Subscriber에게 전달합니다.
+// Publisher ---- stream of data ----> Subscriber
+
+
+// MARK: - 6. What is Combine Framework?
+// combine framework는 비동기 이벤트를 처리하는 reactive framework입니다.
+
+// MARK: - 5. What is Functional Programming
 // 기존 명령형 프로그래밍에서는 변수를 지정하고 그 변수의 상태를 바꾸기 위해서는 별도 위치에서 다른 값을 할당하는 등의 동작이 필요하다.
 // Imparative(명령형) 프로그래밍과 달리, functional(함수형) 프로그래밍은 상태가 바뀌는 값이 immutable하게 구성될 수 있다.
 // 1) 함수형 프로그래밍은 race condition, dead lock 등의 문제점을 해소할 수 있다.
--- HelloCombine/ViewController.swift
+++ HelloCombine/ViewController.swift
@@ -12,8 +12,20 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    // imparative programming (명령형 프로그래밍) 예시
+    let notification = Notification.Name("MyNotification")
+    let center = NotificationCenter.default
+    
+    // notification 옵저버 추가
+    let observer = center.addObserver(forName: notification, object: nil, queue: nil) { notification in
+      print("Notification received-!!")
+    }
+    
+    // notification 이벤트 전달
+    center.post(name: notification, object: nil)
+    
+    // notification 옵저버 제거
+    center.removeObserver(observer)
   }
-
-
 }
 
