c5f57f542fe3d888bc66ac75404b8993b56d4936
applebuddy <eagermin92@gmail.com>
2022-09-25 03:08:32 +0900

    Study lecture 12. Subjects

--- HelloCombine/ViewController.swift
+++ HelloCombine/ViewController.swift
@@ -10,6 +10,10 @@ import RxSwift
 import RxCocoa
 import Combine
 
+enum MyError: Error {
+  case subscriberError
+}
+
 class StringSubscriber: Subscriber {
   // Publisher            Subscriber 의 상호 관계
   // <-------- subscribes
@@ -21,32 +25,64 @@ class StringSubscriber: Subscriber {
   // subscribe 이후, publisher로부터 subscription을 수신했을때 호출 됩니다.
   func receive(subscription: Subscription) {
     print("Received Subscription")
-    subscription.request(.max(3)) // publiser야 최대 3개의 값만 줘봐
+    subscription.request(.max(2)) // publiser야 최대 3개의 값만 줘봐
   }
   
   // publisher로부터 input 값을 수신했을때 호출됩니다.
   func receive(_ input: String) -> Subscribers.Demand {
     print("Received value : \(input)")
     return .none // publisher한테 더이상 받을 것 없어
+//    return .max(1)
 //    return .unlimited // 줄 수 있는거 다 받을래
   }
   
   // publisher가 publish 이벤트를 마쳤을때 호출 됩니다.
-  func receive(completion: Subscribers.Completion<Never>) {
+  func receive(completion: Subscribers.Completion<MyError>) {
     print("Completed")
   }
 
   typealias Input = String
-  typealias Failure = Never // Failure를 Never로 지정하면 fail이 발생하지 않습니다.
+  typealias Failure = MyError // Failure를 Never로 지정하면 fail이 발생하지 않습니다.
 }
 
 class ViewController: UIViewController {
   
   private let disposeBag = DisposeBag()
+//  private let cancellables = Set<Cancellable>()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // MARK: - 12. Subjects
+    // Subjects : Subject는 이벤트를 방출할 수 있는 Publisher이면서, 구독이 가능한 Subscriber도 될 수 있습니다.
+    // * RxSwift의 Subject는 이벤트를 방출할 수 있는 Observable이면서 동시에 구독이 가능한 Observer입니다.
+    // - Publisher
+    // - Subscriber
+    let subscriber = StringSubscriber()
+    let subject = PassthroughSubject<String, MyError>()
+    // subject는 Publisher처럼 구독이 가능합니다. 즉, Subscriber가 구독하여 이벤트를 받을 수 있으며, subject에서 원할때 이벤트를 Subscriber로 보낼 수 있습니다.
+    // 1) Subscriber는 Subject를 구독할 수 있습니다.
+    subject.subscribe(subscriber)
+
+    // 2) Subject는 이벤트를 방출할 수도 있습니다.
+    let subscription = subject.sink { completion in
+      print("Received Completion from sink")
+    } receiveValue: { value in
+      print("Received Value from sink, value : \(value)")
+    }
+
+    subject.send("A") // Received value : A
+    subject.send("B") // Received value : B
+    subject.send("C") // .max(2)로 request를 했을 경우, 세번째 이벤트인 C는 방출되지 않습니다. input을 받을때 .none 대신 .max(1)을 반환하면 이벤트를 추가로 받을 수 있다.
+    subject.send("B")
+    
+    subscription.cancel()
+    
+    subject.send("ㅜㅜ") // 구독이 취소되면 이후 이벤트는 방출되지 않아요.
+    subject.send("ㅠㅠ")
+    
+    // MARK: - 11. Implementing a Subscriber
+    /*
     let publisher = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"].publisher
     let subscriber = StringSubscriber()
     publisher.subscribe(subscriber)
@@ -57,6 +93,7 @@ class ViewController: UIViewController {
     Received value : A
     Received value : B
     Received value : C
+     */
      */
 
     // MARK: - 9. Sending Notifications Using Publisher and Subscriber
