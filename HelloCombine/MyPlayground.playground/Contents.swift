0ad6d007150dd6c470f172cf8ce491c1f9bd8d99
applebuddy <eagermin92@gmail.com>
2022-09-25 22:41:53 +0900

    Study lecture 16 ~ 18. map, map with KeyPath, flatMap operator

--- HelloCombine/MyPlayground.playground/Contents.swift
+++ HelloCombine/MyPlayground.playground/Contents.swift
@@ -1,16 +1,90 @@
 import UIKit
 import Combine
 
+// MARK: 18. flatMap operator
+// flatMap operator는 많은 transformation operator들 중 중요한 operator 중 하나입니다.
+// flatMap operator를 사용하면 다수의 업 스트림 publisher들을 하나의 downstream publisher로 변환할 수 있습니다.
+struct School {
+  let name: String
+  let noOfStudents: CurrentValueSubject<Int, Never>
+  
+  init(name: String, noOfStudents: Int) {
+    self.name = name
+    self.noOfStudents = CurrentValueSubject(noOfStudents)
+  }
+}
+
+let citySchool = School(name: "Fountain Head School", noOfStudents: 100)
+let school = CurrentValueSubject<School, Never>(citySchool)
+/*
+school.sink {
+  // 구독하면서 초기 School 값 수신
+  print($0)
+}
+*/
+
+let anyCancellable = school.flatMap {
+  // flatMap으로 내부 Publication event들을 감지하고 수신할 수 있다.
+  // noOfStudents CurrentValueSubject를 구독하여 이벤트를 받을 수 있다.
+  $0.noOfStudents
+}
+.eraseToAnyPublisher()
+.sink {
+  print($0)
+}
+
+
+let townSchool = School(name: "Town School", noOfStudents: 101)
+// citySchool.noOfStudents.value += 1 // noOfStudyents.value를 바꾸어도 이벤트가 방출되지 않음
+
+school.value = townSchool // 구독한 school CurrentValueSubject의 value를 바꾸니 이벤트가 수신
+// flatMap을 활용해서 noOfStudents CurrentValueSubject를 구독하여 값이 변화할때 이벤트를 받을 수 있게 되었다.
+townSchool.noOfStudents.value = 0
+townSchool.noOfStudents.value += 100
+townSchool.noOfStudents.value -= 100
+
+// MARK: 17. map KeyPath
+// map operator에서 KeyPath를 사용하여 structure의 개별 값들을 접근하여 다룰 수 있습니다.
+/*
+struct Point {
+  let x: Int
+  let y: Int
+}
+
+let publisher = PassthroughSubject<Point, Never>()
+publisher.map(\.x, \.y).sink(receiveValue: { x, y in
+  print("x is \(x) and y is \(y)")
+})
+
+publisher.send(Point(x: 1, y: 2))
+publisher.send(Point(x: 10, y: 20))
+ */
+
+// MARK: 16. map operator
+// ex) [100, 23] -> ["one hundred and twenty three"]로 변환하는 방법?
+/*
+let formatter = NumberFormatter()
+formatter.numberStyle = .spellOut
+
+[213, 45, 67].publisher.map {
+  // map operator를 통해 Sequence에 있는 각각의 elements를 특정 연산하여 또다른 Sequence를 반환할 수 있다.
+  formatter.string(from: NSNumber(integerLiteral: $0))
+}.sink { element in
+  print(element)
+}
+*/
+
 // MARK: 15. collect operator
 // collect operator는 방출할 모든 이벤트를 하나로 모아놓은 Array로 반환한다.
 // collect N: Int 인자를 넣으면 N개 단위로 나누어서 Array를 반환한다.
-
+/*
 let anyCancellable = ["A", "B", "C", "D"].publisher.collect(3)
   .sink { element in
     print(element)
   }
 
 anyCancellable.cancel()
+ */
 
 // MARK: - 14. Understanding Transforming Operators
 // 기존 Sequence를 각각의 elemenet에 대하여 특정연산을 적용한 새로운 Sequence로 변환시킨다.
