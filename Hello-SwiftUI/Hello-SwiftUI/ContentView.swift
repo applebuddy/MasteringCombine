e31e608a6ebd2ec76312ea4db9a89fdd2e34e967
applebuddy <eagermin92@gmail.com>
2022-09-28 02:09:07 +0900

    Study lecture 72. Hello SwiftUI
    can create SwiftUI Project and use preview function without debugging

---
+++ Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Hello-SwiftUI
+//
+//  Created by MinKyeongTae on 2022/09/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  // if you use SwiftUI, you can view your preview before debugging.
+  var body: some View {
+    Text("The Complete iOS 13 Course")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Hello-SwiftUI/Hello-SwiftUI/Hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftUIApp.swift
+//  Hello-SwiftUI
+//
+//  Created by MinKyeongTae on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
