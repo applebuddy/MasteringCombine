fb2b884e2c85f9f3fb6962a3e1201343178b56b8
applebuddy <eagermin92@gmail.com>
2022-09-28 02:27:38 +0900

    Study lecture 73 ~ 74 . Customizing Text and Combining Views Using Stacks, Displaying Images Practice using SwiftUI framework

--- Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
+++ Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
@@ -5,13 +5,37 @@
 //  Created by MinKyeongTae on 2022/09/28.
 //
 
+// MARK: - Secion 13. SwiftUI Basics
+// MARK: 71 ~ 73. SwiftUI Basic function Practice
+
 import SwiftUI
 
 struct ContentView: View {
   // if you use SwiftUI, you can view your preview before debugging.
   var body: some View {
-    Text("The Complete iOS 13 Course")
-      .padding()
+    VStack(alignment: .center) {
+      Image("icon")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .padding(.horizontal, 30.0)
+        .cornerRadius(50.0)
+//        .clipShape(Circle()) // 둥글게 모양을 잘라내어 보이게하려면 사용
+      
+      Text("The Complete iOS 13 Course")
+        .font(.largeTitle)
+        .foregroundColor(.green)
+        .multilineTextAlignment(.center)
+      Text("Second Line")
+        .font(.title)
+        .foregroundColor(.orange)
+      
+      HStack {
+        Text("Left Side")
+        Text("Right Side")
+          .padding(.all)
+      }
+    }
+    .ignoresSafeArea(.all, edges: .top)
   }
 }
 
