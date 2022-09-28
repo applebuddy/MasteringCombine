523bafa13bd2ed809a4670f9322df7591bf49665
applebuddy <eagermin92@gmail.com>
2022-09-29 02:41:18 +0900

    Study lecture 90. Retrieving Stories by ID

---
+++ HackerNews/HackerNews/Models/Story.swift
@@ -0,0 +1,14 @@
+//
+//  Story.swift
+//  HackerNews
+//
+//  Created by MinKyeongTae on 2022/09/29.
+//
+
+import Foundation
+
+struct Story: Codable {
+  let id: Int
+  let title: String
+  let url: String
+}
--- HackerNews/HackerNews/Services/Webservice.swift
+++ HackerNews/HackerNews/Services/Webservice.swift
@@ -9,6 +9,18 @@ import Foundation
 import Combine
 
 class Webservice {
+  
+  func getStoryById(storyId: Int) -> AnyPublisher<Story, Error> {
+    guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(storyId).json?print=pretty") else {
+      fatalError("Invalid URL")
+    }
+    return URLSession.shared.dataTaskPublisher(for: url)
+      .map(\.data)
+      .decode(type: Story.self, decoder: JSONDecoder())
+      .catch { _ in Empty<Story, Error>() } // error 발생 시 Empty publisher를 반환
+      .eraseToAnyPublisher()
+  }
+  
   func getAllTopStories() -> AnyPublisher<[Int], Error> {
     guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty") else {
       fatalError("Invalid URL")
