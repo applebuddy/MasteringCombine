29ad4ed7888c0bc23ffb65c92c7afa669844d60d
applebuddy <eagermin92@gmail.com>
2022-09-29 20:53:31 +0900

    Review WeatherApp implementation way and descriptions related to that

--- WeatherApp/WeatherApp/Services/WebService.swift
+++ WeatherApp/WeatherApp/Services/WebService.swift
@@ -15,9 +15,10 @@ final class WebService {
     }
     
     return URLSession.shared.dataTaskPublisher(for: url)
-      .map(\.data)
-      .decode(type: WeatherResponse.self, decoder: JSONDecoder())
-      .map { $0.main }
+      .map(\.data) // KeyPath를 통해 data, response 중 data만 downstream으로 이동
+      .decode(type: WeatherResponse.self, decoder: JSONDecoder()) // WeatherResponse로 디코딩
+      .map { $0.main } // WeatherRespons의 main: Weather 필드 데이터만 downstream으로 이동
+      .catch { _ in Empty<Weather, Error>() } // 에러 발생 시 Empty 타입으로 반환할 수 있다.
       .receive(on: RunLoop.main) // main thread에서 동작하도록 합니다.
       .eraseToAnyPublisher() // 최종적인 형태로 데이터를 전달할때 eraseToAnyPublisher를 사용할 수 있다.
   }
--- WeatherApp/WeatherApp/Utiils/Constants.swift
+++ WeatherApp/WeatherApp/Utiils/Constants.swift
@@ -9,6 +9,10 @@ import Foundation
 
 struct Constants {
   static func weather(city: String) -> String {
-    return "http://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=83ac5aa4b3c35a75018e9ffe83d7060d&units=imperial"
+    return "http://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(self.appId)&units=imperial"
   }
+  
+  static let appId = "83ac5aa4b3c35a75018e9ffe83d7060d"
 }
+
+
